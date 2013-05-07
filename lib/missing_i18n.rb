fb5222ad960599b972cf3ff0865cbd03da2470eb
kikito <kikito@gmail.com>
2013-05-07 23:26:51 +0200

    added default ignored scopes for rails_i18n

--- lib/missing_i18n.rb
+++ lib/missing_i18n.rb
@@ -3,10 +3,20 @@ require "missing_i18n/engine"
 module MissingI18n
   DEFAULT_IGNORED_SCOPES = %w{
     activemodel.errors.messages
+    activemodel.errors.template.header
     activerecord.errors.messages
+    activerecord.errors.template.header
+    datetime.distance_in_words
+    datetime.prompts
     errors.messages
+    errors.template.header
     helpers.button
+    helpers.select
+    helpers.submit
+    i18n
     number
+    support.array
+    time
   }
 
   def self.ignored_scopes=(new_ignored_scopes)
